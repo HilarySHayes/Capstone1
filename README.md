fcbf9f6dd4ee7f9c99525c0e73121da92dfd91dd
Hilary Hayes <sarneski@gmail.com>
2021-02-23 12:29:00 -0700

    Started readme, added neighborhoods with higher crime rates

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Capstone1
+
+# Background
+
+From covid to discussions of police funding, 2020 ...
+
+
+# Data
+
+The dataset includes the last five years of criminal offenses in the City and County of Denver, Colorado. Based on the National Incident Based Reporting System, the dataset is available publically online [here](https://www.denvergov.org/opendata/dataset/city-and-county-of-denver-crime). The data includes more than 451,400 reported crimes from 2016-2020. Each crime is classified by offense type, includes the date of occurence, and reporting date. Most reported crimes also include the neighborhood and geographical location coordinates. In accordance with legal protections against identifying sexual assault victims, addresses are not included. Child abuse cases and all crimes involving juveniles are not reported in this dataset at all. 
+
+Insert cloropleth map???
+
+# Exploratory Data Analysis
+
+Looking at a barchart showing how categories of crime have changed over time, the categories of traffic accidents, theft from motor vehicules, auto theft, and drug 
+
+![Barplot of Crime in Denver By Crime Category](./images/DenverCrimeByCategoryBarplot.png)
+
+## Where do most crimes occur?
+
+![](./images/DenverTopNCrimeNeighborhoods.png)
+
+
+# Future directions
--- src/crime.py
+++ src/crime.py
@@ -22,8 +22,24 @@ class CrimeDataFrame():
         self.df['month'] = self.df[date_col].dt.month
         self.df['week'] = self.df[date_col].dt.week
 
+    def barplot_city_crime_by_category(self, offense_category_id, incident_id, city, palette):
+        fig, ax = plt.subplots(figsize=(18,10))
+        sns.set_palette(palette)
+        cat_by_year = self.df.groupby(['year', offense_category_id]).agg({incident_id:'count'}).reset_index()
+        ax = sns.barplot(x=offense_category_id, y=incident_id, hue='year', data=cat_by_year[cat_by_year.year!=2021].sort_values(incident_id, ascending=False), saturation=0.7, alpha=0.9, edgecolor='black')
+        ax.set_yticklabels([int(x) for x in ax.get_yticks()], size=20)
+        plt.ylabel('# of Occurences Per Year', size=22)
+        plt.xlabel('')
+        plt.setp(ax.patches, linewidth=1)
+        plt.title(f'{city} Crime By Category', fontsize=30)
+        plt.xticks(rotation = 30, ha='right', fontsize=18)
+        plt.tight_layout()
+        plt.legend(fontsize='xx-large')
+        fig.savefig(f'../images/{city}CrimeByCategoryBarplot.png')
+
     def plot_specific_category_over_time(self, ax, specific_category, offense_category_id, first_offense_date, incident_id):
         cat_df = self.df[self.df[offense_category_id] == specific_category].copy()
+        cat_df = cat_df[cat_df.year != 2021]
         cat_df = cat_df.groupby(['year', 'month']).agg({incident_id: 'count'}).rename(columns={incident_id:'incident_count'}).reset_index()
         
         n =  max(cat_df.year) - min(cat_df.year) + 1
@@ -47,14 +63,14 @@ class CrimeDataFrame():
     def plot_all_cats_over_time(self, offense_category_id, first_offense_date, incident_id, city):
         categories = self.df[offense_category_id].unique()
         num_rows = int(np.ceil(len(categories)/2))
-        fig, axes = plt.subplots(num_rows, 2, figsize=(24, num_rows*6))
+        fig, axes = plt.subplots(num_rows, 2, figsize=(18, num_rows*8))
         for category, ax in zip(categories, axes.flatten()):
             self.plot_specific_category_over_time(ax, category, offense_category_id, first_offense_date, incident_id)
         if len(categories) != num_rows * 2:
             axes.flatten()[-1].axis('off')
         fig.suptitle(f'{city} Crime over Time', x=0.5, y=1.01, fontsize=35, fontweight='bold')
         plt.tight_layout()
-        fig.savefig(f'../images/{city}_Crime_over_Time.png')     
+        fig.savefig(f'../images/{city}_Crime_over_Time.png', bbox_inches='tight')     
 
     def boxplots_by_cat(self, offense_category_id):
         box_df = self.df[self.df.year != 2021].copy()
@@ -68,6 +84,7 @@ class CrimeDataFrame():
             f.set(xlabel='Year',ylabel='Number of Incidents Per Week')
             f.set_title(f'Distribution of {capitalize_titles(category)} Per Week From 2016 - 2020')
             plt.tight_layout()
+            plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9, wspace=0, hspace=0)
             fig.savefig('../images/Denver_Boxswarm_By_Cat.png')
 
     def kdeplots_by_cat(self, offense_category_id):
@@ -83,9 +100,27 @@ class CrimeDataFrame():
         plt.tight_layout()
         fig.savefig('../images/Denver_KDEplots_By_Cat.png')
 
+    def top_crime_neighborhoods(self, n, neighborhood_id, city):
+        fig, ax = plt.subplots(figsize=(9, max(n//3, 1)))
+        self.df = self.df[(self.df.year != 2021) & (self.df[neighborhood_id] != 'cbd')]
+        neighborhood = self.df.groupby(neighborhood_id).size().sort_values(ascending=False).reset_index()
+        neighborhood.rename(columns={neighborhood_id:'Neighborhood', 0:'Count'}, inplace=True)
+        top_n = neighborhood[:n]
+        g = sns.barplot(x='Count', y='Neighborhood', data=top_n, palette='mako', edgecolor='black')
+        g.axes.set_title(f"Neighborhoods in {city} with Most Crime From 2016-2020",fontsize=16)
+        g.set_xlabel("Number of Crime Occurrences",fontsize=12)
+        g.set_ylabel("")
+        g.set_yticklabels([capitalize_titles(y.get_text()) for y in g.get_yticklabels()])
+        g.tick_params(labelsize=11)
+        plt.tight_layout()
+        fig.savefig(f'../images/{city}TopNCrimeNeighborhoods.png')
+
+
 if __name__ == '__main__':
     Denver = CrimeDataFrame('../data/denver_crime.csv', ['FIRST_OCCURRENCE_DATE', 'LAST_OCCURRENCE_DATE', 'REPORTED_DATE'], "%m/%d/%Y %I:%M:%S %p", 'FIRST_OCCURRENCE_DATE')
     print(Denver.df.info())
     #Denver.plot_all_cats_over_time('OFFENSE_CATEGORY_ID','FIRST_OCCURRENCE_DATE', 'INCIDENT_ID', 'Denver')
     #Denver.boxplots_by_cat('OFFENSE_CATEGORY_ID')
-    Denver.kdeplots_by_cat('OFFENSE_CATEGORY_ID')
+    #Denver.kdeplots_by_cat('OFFENSE_CATEGORY_ID')
+    #Denver.barplot_city_crime_by_category('OFFENSE_CATEGORY_ID', 'INCIDENT_ID', 'Denver', ["#1D3557","#457B9D","#A8DADC","#F3C6C6","#E63946"])    
+    Denver.top_crime_neighborhoods(15, 'NEIGHBORHOOD_ID', 'Denver')
